95a33e3258340c02569ac7210c9f821bd09f2932
dMordas <dontbeatmybrain@gmail.com>
2019-11-21 23:26:03 +0300

    First commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float _speed = 4f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(Vector3.down * _speed * Time.deltaTime);
+       if (transform.position.y < -5)
+       {
+           float randomX = Random.Range(-8f, 8f);
+           transform.position = new Vector3(randomX,5,0);
+       }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Debug.Log("Collider 1");
+        if (other.tag == "Laser")
+        {
+            Debug.Log("Collider Laser");
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Player")
+        {Debug.Log("Collider player");
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float _laserSpeed = 10f;
+        
+
+    void Update()
+    {
+       transform.Translate(Vector3.up * Time.deltaTime * _laserSpeed); 
+       if(transform.position.y >8f)
+           Destroy(this.gameObject);
+    }
+}
