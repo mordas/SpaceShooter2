163f62c18c6671ff10a4ca20a9b4e1b2a9d1d86e
dMordas <dontbeatmybrain@gmail.com>
2019-12-01 23:04:37 +0300

    Enemy Destroy animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,9 +8,19 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _speed = 4f;
     [SerializeField] private GameObject _player;
 
+    private Animator _deadAnim;
     void Start()
     {
         _player = GameObject.FindWithTag("Player");
+        if (_player == null)
+        {
+            Debug.Log("Player is null");
+        }
+        _deadAnim = gameObject.GetComponent<Animator>();
+        if (_deadAnim == null)
+        {
+            Debug.Log("Anim is null");
+        }
     }
 
     // Update is called once per frame
@@ -29,8 +39,10 @@ public class Enemy : MonoBehaviour
         Debug.Log("Collider 1");
         if (other.tag == "Laser")
         {
-            Destroy(other.gameObject);
-            Destroy(this.gameObject);
+           // Destroy(other.gameObject);
+//            Destroy(this.gameObject);
+            //_deadAnim.SetBool("isDestroy",true);
+            StartCoroutine(enemyDestroy());
             if (_player)
             {
            _player.GetComponent<Player>().SetScore(10); 
@@ -39,8 +51,9 @@ public class Enemy : MonoBehaviour
 
         if (other.tag == "Player")
         {
-            Debug.Log("Collider player");
-            Destroy(this.gameObject);
+            StartCoroutine(enemyDestroy());
+//            _deadAnim.SetBool("isDestroy",true);
+//            Destroy(this.gameObject);
             Player player = other.transform.GetComponent<Player>();
             if (player != null)
             {
@@ -48,5 +61,13 @@ public class Enemy : MonoBehaviour
                 player.Damage();
             }
         }
+    }
+
+    IEnumerator enemyDestroy()
+    {
+        _speed = 0.5f;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            _deadAnim.SetBool("isDestroy",true);
+            yield return new WaitForSeconds(1);
     }
 }
